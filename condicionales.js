5e01c6b41c5bd518d517885abeb576458aaec641
jfernand <jfernand196@gmail.com>
2021-11-03 16:08:03 -0500

    First excercices with conditionals

---
+++ condicionales.js
@@ -0,0 +1,62 @@
+// Objetivo: escribir un programa que imprima "Es mayor que 10" 
+// si el número asignado a la variable number es mayor que 10.
+
+// let number = 11
+
+// if(number>10){ 
+//     console.log("the number is greater than 10")
+// }
+// else{
+//     console.log("the number is less than 10")
+// }
+
+// Objetivo: escribir un programa que imprima El número es igual a 8 si el número asignado a la variable 
+// number es 8, e imprima No es 8 si es diferente de 8.
+
+// let number = 2
+
+// if(number === 8){
+//     console.log("the number is 8")
+// }
+// else{
+//     console.log("the number is different from 8")
+// }
+// Objetivo: Escribir nuevamente el programa del reto anterior, utilizando la sintaxis del operador 
+// condicional terniario.
+// let number = 8
+
+// number === 8 ? console.log("the number is 8"):console.log("the number is different from 8");
+
+// Objetivo: Escribir un programa que imprima Buenos días si el número asignado a la variable time es
+//  menor que 12. Si el numero es mayor o igual a 12 y menor o igual a 18, el programa debe imprimir 
+//  Buenas tardes, de lo contrario debe imprimir Buenas noches.
+
+// let time = 19
+// if(time<12){
+//     console.log("buenos dias")
+// } 
+// else if(time >= 12 & time <=18){
+//     console.log("Buenas tardes")
+// }
+// else{
+//     console.log("Buenas noches")
+// }
+
+// Objetivo: Escribe un programa que imprima en la consola Está listo, si el nombre de la persona es 
+// María o Pedro. Si las condiciones no se cumplem, el programa debe finalizar.
+
+// let name = "Maria"
+
+// if(name === "Maria" || name==="Pedro"){ 
+// console.log("Esta listo")}
+
+// Objetivo: Decide si las siguietes expresiones booleanas evaluan falso o verdadero:
+console.log(3 != 4 && !("pedro" === "juan" || 26 > 10)) // false
+console.log(true && !(false || true)) // false
+console.log(true && !true) // false
+console.log(true && false) // false
+console.log(false) // false
+console.log(true) //true
+console.log(!(5 === 5) && 8 !== 8) //false
+console.log(("cat" === "kat" && 26 > 30) || ("cat" === "cat" && 26 > 10)) // true
+console.log(!("testing" == "testing" && !(5 > 8))) // false
