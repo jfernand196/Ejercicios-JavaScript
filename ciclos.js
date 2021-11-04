3bf0112f12d7fad93d9a7ae77ebce9e61b55b2bc
jfernand <jfernand196@gmail.com>
2021-11-03 21:56:44 -0500

    my first excercices with cicles

---
+++ ciclos.js
@@ -0,0 +1,56 @@
+// Objetivo: Crear un programa que imprima en la consola 50 veces el texto Estoy aprendiendo.
+
+// Cada vez que se imprime el texto, debe aparecer al inicio el número de veces que se ha impreso:
+
+// opcion 1
+// let i = 0
+// while(i <= 50){
+//     console.log(i + " Estoy aprendiendo")
+//     i++
+// }
+// opcion 2
+// for(let i = 0; i<=50; i++){
+//     console.log(i + " Estoy aprendiendo")
+// }
+
+// Objetivo: Corregir el siguiente código para que se imprima 5 veces el texto que se encuentra dentro del
+//  cuerpo del ciclo.
+// while (false) {
+//     console.log("Hola desde el cuerpo del ciclo");
+//   }
+// correccion:
+// let i= 1
+// while (i<=5) {
+//         console.log(i + " Hola desde el cuerpo del ciclo");
+//         i++
+//       }
+// Objetivo: Corregir el siguiente programa para que no entre en un cilco infinito, sino que imprima 2 
+// veces el contenido del cuerpo del ciclo.
+
+// while (true) {
+//   console.log("Hola desde el cuerpo del ciclo");
+// }
+// correccion:
+// let i = 1
+// while (i<=2) {
+//   console.log("Hola desde el cuerpo del ciclo");
+//   i++
+// }
+
+// Objetivo: Reescribe el programa del primer reto de esta sección utilizando la expresión de 
+// JavaScript for.
+// for(let i = 0; i<=50; i++){
+//     console.log(i + " Estoy aprendiendo")
+// }
+// Objetivo: Corrigir el siguiente código para que en la consola se imprima 5 veces Estoy aprendiendo 
+// JavaScript
+
+// for (var i = 0; i < 5; i ++) {
+//   console.log("Hola mundo");
+//   i++
+// }
+// correcion:
+for (var i = 0; i < 5; i ++) {
+  console.log("Hola mundo");
+  
+}
