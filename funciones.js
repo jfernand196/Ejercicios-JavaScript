c9471d2f7e2f8d685e0a5f2c890a8ac9401c9836
jfernand <jfernand196@gmail.com>
2021-11-03 22:13:46 -0500

    my first excercice with functions

---
+++ funciones.js
@@ -0,0 +1,46 @@
+// Objetivo: Define una función llamada greetings, que reciba cómo parámetros el nombre y el apellido de 
+// una persona y retorne lo siguiente:
+
+// Mi nombre es Nayib y mi apellido es Abdalá
+// Mi nombre es Germán y mi apellido es Escobar
+// Mi nombre es Simón y mi apellido es Hoyos
+
+// function greetings (name, lastname){
+//     return "Mi nombre es " + name + " y mi apellido es " + lastname;
+// }
+
+// console.log(greetings("Nayib", "Abdalá"));
+// console.log(greetings("Germán", "Escobar"));
+// console.log(greetings("Simón", "Hoyos"));
+
+// Objetivo: Escribir una función llamada suma que reciba dos números y retorne su suma.
+
+// function sum(x, y){
+// return x + y
+// }
+
+// console.log(sum(10, 20));  // 30
+// console.log(sum(10, 10));  // 20
+// console.log(sum(40, 60));  // 100
+// console.log(sum(50, 200));  // 250
+
+// Objetivo: Modificar el siguiente código para que imprima el resultado esperado.
+
+// Código actual:
+
+// function sayMyName(name) {
+//   return ("Tu nombre es " + name);
+// }
+
+// var name = "Nayib";
+// sayMyName(name);
+
+// correccion:
+
+function sayMyName(name) {
+  return ("Tu nombre es " + name);
+}
+
+var name = "Nayib";
+console.log(sayMyName(name))
+
